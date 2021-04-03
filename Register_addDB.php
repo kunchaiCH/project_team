1eeca6ec504761887f0150222fb70af75808bf9c
TOR <torthanakorn2540@gmail.com>
2021-04-03 14:13:01 +0700

    tor

--- Register_addDB.php
+++ Register_addDB.php
@@ -1,31 +1,45 @@
 <?php
 include('condb.php');
 
+$username = $_POST['username'];
 $ชื่อ = $_POST['ชื่อ'];
 $นามสกุล = $_POST['นามสกุล'];
-$อาชีพ = $_POST['อาชีพ'];
+$ประเภท = $_POST['ประเภท'];
 $ความสามารถ = $_POST['ความสามารถ'];
 $ราคา = $_POST['ราคา'];
 $ที่อยู่ = $_POST['ที่อยู่'];
 $เบอร์โทร = $_POST['เบอร์โทร'];
+$role = $_POST['role'];
 $email = $_POST['email'];
 $password = $_POST['password'];
 
 $sql ="INSERT INTO นายช่าง
     
-    (ชื่อ, นามสกุล, อาชีพ, ความสามารถ, ราคา, ที่อยู่, เบอร์โทร, email, password) 
+    (username, ชื่อ, นามสกุล, ประเภท, ความสามารถ, ราคา, ที่อยู่, เบอร์โทร) 
 
     VALUES 
 
-    ('$ชื่อ', '$นามสกุล', '$อาชีพ', '$ความสามารถ', '$ราคา', '$ที่อยู่', '$เบอร์โทร', '$email', '$password')";
+    ('$username', '$ชื่อ', '$นามสกุล', '$ประเภท', '$ความสามารถ', '$ราคา', '$ที่อยู่', '$เบอร์โทร')";
+    
+    $result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
+    // mysqli_close($con);
+    
+
+$sql ="INSERT INTO masterlogin
+    
+    (username, email, password, role) 
+
+    VALUES 
+
+    ('$username', '$email', '$password', '$role')";
     
     $result = mysqli_query($con, $sql) or die ("Error in query: $sql " . mysqli_error());
     mysqli_close($con);
-    
+
     if($result){
       echo "<script>";
       //echo "alert('Add Succesfuly');";
-      echo "window.location ='index.php'; ";
+      echo "window.location ='admin_home.php'; ";
       echo "</script>";
     } else {
       
